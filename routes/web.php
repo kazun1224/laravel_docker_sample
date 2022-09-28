bf7712ff728fe98e697e23dedda133965ff8a491
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-28 23:21:51 +0900

    laravel auth を追加

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
